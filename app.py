fbdda2554ea04609c5095a04102b30ef6660ffb6
lugocamilo <145741193+lugocamilo@users.noreply.github.com>
2026-03-31 16:06:47 -0500

    modulo login pantalla
    Se hizo el html y css del login, se necesita el logo volverlo de igual tamaño como en el mockup

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    # Flask buscará automáticamente en la carpeta 'templates'
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
